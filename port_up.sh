7a61050bcb316cbe7a5a53be96c2051ab5156e49
RaydenX93 <26058845+RaydenX93@users.noreply.github.com>
2026-04-05 19:01:49 +0200

    Add files via upload

---
+++ port_up.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PORT=$1
+IFACE=$2
+
+# Wait for qBittorrent WebUI to be ready
+while ! wget -qO- http://127.0.0.1:8080/api/v2/app/version >/dev/null 2>&1; do
+    echo "PortUpdateScript: Waiting for qBittorrent WebUI to start..."
+    sleep 5
+done
+
+echo "PortUpdateScript: qBittorrent is ready. Setting listening port to $PORT"
+
+wget -O- -nv --retry-connrefused \
+  --post-data "json={\"listen_port\":${PORT},\"current_network_interface\":\"${IFACE}\",\"random_port\":false,\"upnp\":false}" \
+  http://127.0.0.1:8080/api/v2/app/setPreferences
+
+echo "PortUpdateScript: Port updated successfully."
